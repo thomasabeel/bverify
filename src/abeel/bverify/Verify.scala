569a1c54acb478ef8fb457a968db95505870424a
Thomas Abeel <thomas@abeel.be>
2014-04-11 16:12:17 -0400

    added recursive option added multiple patterns option

--- src/abeel/bverify/Verify.scala
+++ src/abeel/bverify/Verify.scala
@@ -4,21 +4,22 @@ import java.io.File
 import atk.io.PatternFileFilter
 import java.io.PrintWriter
 import atk.util.Tool
+import scala.collection.mutable.MutableList
+import atk.io.IOTools
 
 object Verify extends Tool {
 
-  case class Config(val pattern: String = ".*.log", input: File = null, output: File = null, clean: Boolean = false)
+  case class Config(val recursive: Boolean = false, val pattern: Seq[String] = Seq(".*.log"), input: File = new File("."), output: File = null, clean: Boolean = false)
 
   def main(args: Array[String]): Unit = {
 
     val parser = new scopt.OptionParser[Config]("java -jar bverify.jar") {
-      opt[String]('p', "pattern") action { (x, c) => c.copy(pattern = x) } text ("File pattern to search and process.")
-      opt[File]('i', "input") required () action { (x, c) => c.copy(input = x) } text ("Input directory where to search for log files.")
+      opt[String]('p', "pattern") unbounded () action { (x, c) => c.copy(pattern = c.pattern :+ x) } text ("File patterns to search and process. You can specify this option multiple times.")
+      opt[File]('i', "input") action { (x, c) => c.copy(input = x) } text ("Input directory where to search for log files. Default is current directory.")
 
-      opt[File]('o', "output") action { (x, c) => c.copy(output = x) } text ("File where you want the output to be written")
-      opt[Unit]('c', "clean") action { (_, c) =>
-        c.copy(clean = true)
-      } text ("Remove log files that completed succesfully")
+      opt[File]('o', "output") action { (x, c) => c.copy(output = x) } text ("File where you want the output to be written. Default is screen")
+      opt[Unit]('c', "clean") action { (_, c) => c.copy(clean = true) } text ("Remove log files that completed succesfully")
+      opt[Unit]('r', "recursive") action { (_, c) => c.copy(recursive = true) } text ("Recursively search all sub-directories")
 
     }
     parser.parse(args, Config()) map { config =>
@@ -28,26 +29,32 @@ object Verify extends Tool {
       val pw = if (config.output == null) new PrintWriter(System.out) else new PrintWriter(config.output)
 
       pw.println(generatorInfo)
+      pw.println("# Pattern\tSuccess\tFailure")
+      for (pattern <- config.pattern) {
+        val files = if(config.recursive) IOTools.recurse(config.input, new PatternFileFilter(pattern))   else config.input.listFiles(new PatternFileFilter(pattern))
 
-      val files = config.input.listFiles(new PatternFileFilter(config.pattern))
+        var good = 0
+        var fail = 0
+        val failureList=MutableList.empty[File]
+        files.map(file => {
+          val success = tLinesIterator(file).take(30).toList.exists(_.contains("Successfully completed."))
 
-      var good=0
-      var fail=0
-      files.map(file => {
-        val success = tLinesIterator(file).take(30).toList.exists(_.contains("Successfully completed."))
+          if (!success) {
+//            pw.println(file + "\tfailed")
+            failureList +=file
+            fail += 1
+          } else {
+            good += 1
+            if (config.clean)
+              file.delete()
+          }
 
-        if (!success) {
-          pw.println(file + "\tfailed")
-          fail += 1
-        } else {
-          good += 1
-          if (config.clean)
-            file.delete()
-        }
-
-      })
-      pw.println("# "+good+"\t runs succeeded")
-      pw.println("# "+fail+"\t runs failed")
+        })
+        pw.println(pattern+"\t"+good+"\t"+fail)
+        pw.println("# Failed runs: \n# "+failureList.mkString("\n# "))
+//        pw.println("# " + good + "\t runs succeeded")
+//        pw.println("# " + fail + "\t runs failed")
+      }
       finish(pw)
 
     }
